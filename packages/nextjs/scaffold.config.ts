655685daebf4888f8b1f1f2a69101f130076107b
Martin Grabina <mgrabina@itba.edu.ar>
2024-07-12 15:14:05 +0200

    double check enviroment for deployments

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,3 +1,4 @@
+import { env } from "process";
 import * as chains from "viem/chains";
 
 export type ScaffoldConfig = {
@@ -8,9 +9,11 @@ export type ScaffoldConfig = {
   onlyLocalBurnerWallet: boolean;
 };
 
+const isProduction = env.NEXT_PUBLIC_VERCEL_ENV === "production";
+
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat, chains.sepolia],
+  targetNetworks: [isProduction ? chains.sepolia : chains.hardhat],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
